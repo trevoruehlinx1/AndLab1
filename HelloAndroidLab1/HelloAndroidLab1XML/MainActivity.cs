1fde9b590d3f01651032bbadc8491be532374a2d
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2018-10-01 10:19:52 -0700

    Both Done!

---
+++ HelloAndroidLab1/HelloAndroidLab1XML/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace HelloAndroidLab1XML
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.activity_main);
+            var aButton = FindViewById<Button>(Resource.Id.aButton);
+            var aLabel = FindViewById<TextView>(Resource.Id.helloLabel);
+            var bLabel = FindViewById<TextView>(Resource.Id.helloLabel2);
+            var clearButton = FindViewById<Button>(Resource.Id.clearButton);
+            clearButton.Text = "Clear";
+            bLabel.SetText(Resource.String.bLabelDefaultText);
+            aButton.Click += (sender, e) => {
+                aLabel.Text = "Hello from the button";
+                bLabel.Text = "Hello from the button";
+            };
+            clearButton.Click += (sender, e) => {
+                aLabel.Text = "";
+                bLabel.Text = "";
+                };
+        }
+    }
+}
+
---
+++ HelloAndroidTutorial/HelloAndroidTutorialXML/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace HelloAndroidTutorialXML
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.activity_main);
+            var aButton = FindViewById<Button>(Resource.Id.aButton);
+            var aLabel = FindViewById<TextView>(Resource.Id.helloLabel);
+            aButton.Click += (sender, e) => {
+                aLabel.Text = "Hello from the button";
+            };
+        }
+    }
+}
+
